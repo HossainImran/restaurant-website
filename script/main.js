6bf9763a87b9092bbc694b95ffb17c44ebcd4e3e
HossainImran <3333mrkhan3333@gmail.com>
2023-07-17 13:18:44 +0600

    all done till section-3

---
+++ script/main.js
@@ -0,0 +1,12 @@
+import '../styles/footer.css';
+import '../styles/header.css';
+import '../styles/hero.css';
+import '../styles/modern-normalize.css';
+import '../styles/section-1.css';
+import '../styles/section-2.css';
+import '../styles/section-3.css';
+import '../styles/section-4.css';
+import '../styles/section-5.css';
+import '../styles/section-6.css';
+import '../styles/style.css';
+
